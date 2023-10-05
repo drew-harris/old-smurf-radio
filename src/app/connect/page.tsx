bf507af3f3738a795796018d83d2573088b6f91c
Drew Harris <harrisd@smu.edu>
2023-10-05 11:42:39 -0500

    Add email

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -5,6 +5,7 @@ export default function Connect() {
     <>
       <h1 className={`${titan.className} text-3xl`}>Connect with S.M.U.R.F!</h1>
       <div className="flex flex-col gap-2 p-2">
+        <a href="mailto:smuradiofrequency@gmail.com">Email</a>
         <a href="https://www.instagram.com/smuradiofrequency/">Instagram</a>
         <a href="https://connect.smu.edu/organization/smurf">SMU 360</a>
         <a href="https://docs.google.com/forms/d/1PK8aNZ6NIcGpKPBOPmd4bLNuRgZjwD6Eisbsdzu2k7U/edit">
