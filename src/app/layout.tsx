5760756bbe1d3cb8e9070088d463ae384d26b5e5
Drew Harris <harrisd@smu.edu>
2023-10-11 09:57:34 -0500

    Add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import MainLayout from "@/components/layout/Mainlayout";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="ga"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-BFZ1L9N2S8"
+      />
+      <Script id="ga2">
+        {`
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BFZ1L9N2S8');
+                `}
+      </Script>
       <body className={inter.className}>
         <MainLayout>{children}</MainLayout>
       </body>
