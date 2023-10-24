82a7edbf0bec13433781bb755dbb70c94685db21
Drew Harris <harrisd@smu.edu>
2023-10-23 20:16:55 -0500

    Update stream link

--- src/app/secret/page.tsx
+++ src/app/secret/page.tsx
@@ -4,7 +4,7 @@ export default function SecretPage() {
   return (
     <div>
       <div className={`pb-7 text-2xl`}>Drews secret audio stream!!!!!</div>
-      <Player streamUrl="https://files.drewh.net/api/public/dl/ZW2MFjHs/Schu.mp3"></Player>
+      <Player streamUrl="https://files.drewh.net/api/public/dl/SWG8YoWf/v4form_w_silence.mp3"></Player>
     </div>
   );
 }
